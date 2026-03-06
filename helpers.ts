5f7ba9f983958db173709f83d1bb19b2d99950ed
ekoli46 <serkanchebi@gmail.com>
2026-03-06 23:46:31 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:46:28.258Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T20:46:30.975Z
+Commit: Fix broken links in documentation
