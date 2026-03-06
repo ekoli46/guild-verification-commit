abae8daad1fd829ed7a63945d978721749bf6df8
ekoli46 <serkanchebi@gmail.com>
2026-03-06 23:47:14 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:46:56.839Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T20:47:13.488Z
+Commit: Fix CSS styling bug on mobile devices
