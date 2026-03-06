5ddc340af319be52aa5fde13ec8608ff20327f05
ekoli46 <serkanchebi@gmail.com>
2026-03-06 23:46:57 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:46:30.975Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T20:46:56.839Z
+Commit: Patch security vulnerability in authentication
