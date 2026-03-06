9ae11f6c13eaa0c9c921e9e117fe840aa8423f93
ekoli46 <serkanchebi@gmail.com>
2026-03-06 23:48:16 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T20:47:48.668Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-06T20:48:16.016Z
+Commit: Add multi-language support
